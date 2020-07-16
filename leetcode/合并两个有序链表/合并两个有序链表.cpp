4d4a6104228a122465fd7167bde5f9b06d49837a
LoveBettygirl <bettycyygirl@163.com>
2020-07-16 23:09:56 +0800

    Add 合并两个有序链表

---
+++ leetcode/合并两个有序链表/合并两个有序链表.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+    {
+        ListNode *head = nullptr;
+        if ((!l1 && l2) || ((l1 && l2) && (l1->val > l2->val)))
+        {
+            head = l2;
+            l2 = l2->next;
+        }
+        else if ((l1 && !l2) || ((l1 && l2) && (l1->val <= l2->val)))
+        {
+            head = l1;
+            l1 = l1->next;
+        }
+        else
+            return head;
+        ListNode *temp = head;
+        while (l1 != nullptr && l2 != nullptr)
+        {
+            if (l1->val <= l2->val)
+            {
+                temp->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;
+        }
+        temp->next = l1 ? l1 : l2;
+        return head;
+    }
+};
+
+int main()
+{
+    return 0;
+}
