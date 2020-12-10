4e93312560f24ef81317e82e40d1de86b9b42332
LoveBettygirl <bettycyygirl@163.com>
2020-12-10 15:26:23 +0800

    Add 删除链表中的节点

---
+++ leetcode/删除链表中的节点/1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        ListNode *temp = node, *prev = nullptr;
+        while (temp->next)
+        {
+            swap(temp->val, temp->next->val);
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = nullptr;
+    }
+};
+
+int main()
+{
+    return 0;
+}
